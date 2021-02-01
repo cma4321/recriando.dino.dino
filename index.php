0859a82b17aad3eff7a8f499bb4b17d55e8d29b6
Caio Moraes <caio.moraes8@hotmail.com>
2021-02-01 19:27:58 -0200

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Dino Dino Game</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js" charset="UTF-8" defer></script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div class="background">
+            <div class="dino">
+                
+            </div>
+            
+        
+        
+        </div>
+    </body>
+</html>
+
